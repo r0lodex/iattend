4f9fd87f521e9693b7f97065f1f412fb2accc870
unknown <niceperson404@gmail.com>
2015-03-30 17:51:24 +0800

    access control

--- app/index.php
+++ app/index.php
@@ -1,3 +1,12 @@
+<?php
+	if(!isset($_SESSION)) {
+		session_start(); 
+	}
+	if( !isset($_SESSION['authorized']) || ($_SESSION['authorized'] == false) ) {
+		header('Location: ../');
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
