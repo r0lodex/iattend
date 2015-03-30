f8bb09b5f04e3f782ae4096dfc5944771be8470e
unknown <niceperson404@gmail.com>
2015-03-30 23:08:47 +0800

    a

--- attendance.sql
+++ attendance.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50541
 File Encoding         : 65001
 
-Date: 2015-03-30 21:41:01
+Date: 2015-03-30 22:48:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -43,13 +43,15 @@ CREATE TABLE `event` (
   `day` date DEFAULT NULL,
   `time` time DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=8192;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=8192;
 
 -- ----------------------------
 -- Records of event
 -- ----------------------------
 INSERT INTO `event` VALUES ('1', 'ceramah', 'minggu orientasi', 'dewan', '2015-03-08', '19:00:00');
 INSERT INTO `event` VALUES ('2', 'bengkel', 'kerja kumpulan', 'makmal', '2015-03-19', '21:00:00');
+INSERT INTO `event` VALUES ('3', 'Skor SPM', 'bengkel kecemerlangan SPM', 'dewan', '2015-03-18', '22:46:19');
+INSERT INTO `event` VALUES ('4', 'orientasi', 'minggu orientasi ambilan julai', 'bangunan dato onn', '2015-03-28', '22:47:46');
 
 -- ----------------------------
 -- Table structure for student
@@ -62,7 +64,7 @@ CREATE TABLE `student` (
   `matrix_no` varchar(25) DEFAULT NULL,
   `serial_no` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=8192;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=8192;
 
 -- ----------------------------
 -- Records of student
