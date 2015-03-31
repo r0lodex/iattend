e81e9587f68690f882f435518a55f4a6740e2adb
Irfan Radzi <rolodex888@gmail.com>
2015-04-01 04:38:23 +0800

    B E A YOU TEE FULL!

--- index.php
+++ index.php
@@ -13,33 +13,34 @@
 </head>
 <body>
 	<div class="container">
-		<hr>
-		<div class="row">
-			<div class="col-md-8">
-				<h1 style="margin-top:0">Hello, World!</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eius veritatis, explicabo magnam itaque at ab nostrum distinctio impedit dolorum dolore ratione repellendus, architecto vitae. Veritatis omnis a sapiente voluptates.</p>
-			</div>
-			<div class="col-md-4">
-			<!-- BEGIN LOGIN FORM -->
-				<h4>i-Attend Login</h4>
-				<p class="text-danger hidden">Invalid username/password</p>
-				<form name="login" action="php/router.php" method="POST">
-					<input type="hidden" value="1" name="login"/>
-					<div class="form-group">
-						<div class="input-group">
-							<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-							<input type="text" name="username" class="form-control" placeholder="Username">
+		<div class="jumbotron">
+			<div class="row">
+				<div class="col-md-8">
+					<h1 style="margin-top:0">Hello, World!</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eius veritatis, explicabo magnam itaque at ab nostrum distinctio impedit dolorum dolore ratione repellendus, architecto vitae. Veritatis omnis a sapiente voluptates.</p>
+				</div>
+				<div class="col-md-4" style="background: #FFF; padding: 20px; border-radius:5px; box-shadow: 0 1px 2px rgba(0,0,0,.2);">
+				<!-- BEGIN LOGIN FORM -->
+					<h4>i-Attend Login</h4>
+					<p class="text-danger hidden">Invalid username/password</p>
+					<form name="login" action="php/router.php" method="POST">
+						<input type="hidden" value="1" name="login"/>
+						<div class="form-group">
+							<div class="input-group">
+								<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+								<input type="text" name="username" class="form-control" placeholder="Username">
+							</div>
 						</div>
-					</div>
-					<div class="form-group">
-						<div class="input-group">
-							<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-							<input type="password" name="password" class="form-control" placeholder="Password">
+						<div class="form-group">
+							<div class="input-group">
+								<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+								<input type="password" name="password" class="form-control" placeholder="Password">
+							</div>
 						</div>
-					</div>
-					<input type="submit" class="btn btn-default btn-sm" value="LOGIN">
-				</form>
-			<!-- END LOGIN FORM -->
+						<input type="submit" class="btn btn-default btn-sm" value="LOGIN &raquo;">
+					</form>
+				<!-- END LOGIN FORM -->
+				</div>
 			</div>
 		</div>
 
