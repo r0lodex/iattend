c259b1fa4def5db09313651396d4358361f80dca
Irfan Radzi <rolodex888@gmail.com>
2015-04-01 02:42:59 +0800

    Initial template for report with print feature.

---
+++ app/report.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>i-Attend Dashboard</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../static/vendor/bootstrap-datepicker.standalone.css">
+	<link rel="stylesheet" type="text/css" href="../static/vendor/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../static/css/iattend.css">
+	<link rel="stylesheet" type="text/css" href="../static/css/iattend_print.css" media="print">
+</head>
+<body>
+	<div class="container">
+		<div class="page-header">
+			<h1><small class="small text-muted">Report</small><br>Event Title <a href="#print" onclick="window.print()" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-print"></span> Print</a></h1>
+		</div>
+		<div>
+			<p>Total Attendees: 100</p>
+
+			<table class="table">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Serial No</th>
+						<th>Student's Name</th>
+						<th>IC No</th>
+						<th>Matrix No</th>
+						<th>Course</th>
+						<th>Time In</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>123</td>
+						<td>Fuk Yu Fuk Mi Tu</td>
+						<td>888888-56-5758</td>
+						<td>YMCA123456</td>
+						<td>Kejuruteraan Konek</td>
+						<td>12.40PM</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
