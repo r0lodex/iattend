df033200533fe46155e33b08c743ab2d96c4aadb
unknown <niceperson404@gmail.com>
2015-03-31 23:47:12 +0800

    a

--- attendance.sql
+++ attendance.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50541
 File Encoding         : 65001
 
-Date: 2015-03-31 03:03:54
+Date: 2015-03-31 23:46:58
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,16 +23,15 @@ CREATE TABLE `attendance` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `student_id` int(11) DEFAULT NULL,
   `event_id` int(11) DEFAULT NULL,
-  `time_reg` time DEFAULT NULL,
+  `time_reg` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=16384;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=16384;
 
 -- ----------------------------
 -- Records of attendance
 -- ----------------------------
-INSERT INTO `attendance` VALUES ('3', '2', '2', '18:06:16');
-INSERT INTO `attendance` VALUES ('4', '2', '1', '18:44:02');
-INSERT INTO `attendance` VALUES ('5', '1', '1', '18:44:25');
+INSERT INTO `attendance` VALUES ('23', '1', '1', '2015-03-31 15:40:28');
+INSERT INTO `attendance` VALUES ('24', '2', '1', '2015-03-31 15:45:51');
 
 -- ----------------------------
 -- Table structure for event
@@ -43,18 +42,16 @@ CREATE TABLE `event` (
   `name` varchar(50) DEFAULT NULL,
   `descp` varchar(255) DEFAULT NULL,
   `venue` varchar(255) DEFAULT NULL,
-  `day` date DEFAULT NULL,
-  `time` time DEFAULT NULL,
+  `day` varchar(25) DEFAULT NULL,
+  `time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=8192;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=8192;
 
 -- ----------------------------
 -- Records of event
 -- ----------------------------
-INSERT INTO `event` VALUES ('1', 'ceramah', 'minggu orientasi', 'dewan', '2015-03-08', '19:00:00');
-INSERT INTO `event` VALUES ('2', 'bengkel', 'kerja kumpulan', 'makmal', '2015-03-19', '21:00:00');
-INSERT INTO `event` VALUES ('3', 'Skor SPM', 'bengkel kecemerlangan SPM', 'dewan', '2015-03-18', '22:46:19');
-INSERT INTO `event` VALUES ('4', 'orientasi', 'minggu orientasi ambilan julai', 'bangunan dato onn', '2015-03-28', '22:47:46');
+INSERT INTO `event` VALUES ('1', 'Ice Breaking', 'Orientation Week', 'Main Hall', '25/03/2015', '1970-01-01 14:00:00');
+INSERT INTO `event` VALUES ('8', 'Cubaan', 'Cubaan', 'Cubaan', '28/03/2015', '1970-01-01 06:30:00');
 
 -- ----------------------------
 -- Table structure for student
@@ -66,11 +63,12 @@ CREATE TABLE `student` (
   `ic` varchar(20) DEFAULT NULL,
   `matrix_no` varchar(25) DEFAULT NULL,
   `serial_no` varchar(50) DEFAULT NULL,
+  `course` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=8192;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=8192;
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES ('1', 'syukri', '111', '222', '333');
-INSERT INTO `student` VALUES ('2', 'kamal', '444', '555', '666');
+INSERT INTO `student` VALUES ('1', 'syukri', '111', '222', '333', 'asd');
+INSERT INTO `student` VALUES ('2', 'kamal', '444', '555', '666', 'qwe');
