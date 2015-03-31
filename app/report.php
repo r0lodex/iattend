b900bdb49c52cd9a1b2854fc924eafdd22432b03
unknown <niceperson404@gmail.com>
2015-04-01 03:46:35 +0800

    report

--- app/report.php
+++ app/report.php
@@ -10,37 +10,7 @@
 </head>
 <body>
 	<div class="container">
-		<div class="page-header">
-			<h1><small class="small text-muted">Report</small><br>Event Title <a href="#print" onclick="window.print()" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-print"></span> Print</a></h1>
-		</div>
-		<div>
-			<p>Total Attendees: 100</p>
-
-			<table class="table">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>Serial No</th>
-						<th>Student's Name</th>
-						<th>IC No</th>
-						<th>Matrix No</th>
-						<th>Course</th>
-						<th>Time In</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>1</td>
-						<td>123</td>
-						<td>Fuk Yu Fuk Mi Tu</td>
-						<td>888888-56-5758</td>
-						<td>YMCA123456</td>
-						<td>Kejuruteraan Konek</td>
-						<td>12.40PM</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+		<?php include '../php/database.php'; include '../php/attendance.php'; attendanceList($_GET); ?>
 	</div>
 </body>
 </html>
--- php/attendance.php
+++ php/attendance.php
@@ -28,12 +28,15 @@ function attendanceRead($input) {
 	);
 
 	// prepare database query
-	$sql = "SELECT std.fullname,std.matrix_no,DATE_FORMAT(CONVERT_TZ(att.time_reg,'+00:00','+08:00'),'%d %b %Y @ %T') as time_reg,att.id,evn.name FROM attendance att join student std ON std.id=att.student_id join event evn on evn.id=:a WHERE att.event_id=:a";
+	$sql = "SELECT std.fullname,std.matrix_no,std.course,DATE_FORMAT(CONVERT_TZ(att.time_reg,'+00:00','+08:00'),'%d %b %Y @ %T') as time_reg,att.id,evn.name FROM attendance att join student std ON std.id=att.student_id join event evn on evn.id=:a WHERE att.event_id=:a";
 	$dbc = Database();
 	$qry = $dbc->prepare($sql);
 	$qry->execute($data);
 	$rows = $qry->fetchAll(PDO::FETCH_ASSOC);
 	$dbc = null;
+	if(isset($input['ret'])){
+		return $rows;
+	}
 	echo json_encode($rows);
 }
 
@@ -52,4 +55,20 @@ function attendanceDelete($input) {
 	$qry = $dbc->prepare($sql);
 	$qry->execute($data);
 	$dbc = null;
+}
+
+function attendanceList($input) {
+	$input['ret'] = true;
+	$res = attendanceRead($input);
+	$i = 1;
+	$total = sizeof($res);
+	$buff = "<div class=\"page-header\">
+		<h1><small class=\"small text-muted\">Report</small><br>".$res[0]['name']." <a href=\"#print\" onclick=\"window.print()\" class=\"btn btn-default btn-xs\"><span class=\"glyphicon glyphicon-print\"></span> Print</a></h1>
+		</div><div><p>Total Attendees: ".$total."</p><table class=\"table\"><thead><tr><th>#</th><th>Student's Name</th><th>Matrix No</th><th>Course</th><th>Time In</th></tr></thead><tbody>";
+	foreach($res as $row){
+		$buff.="<tr><td>".$i."</td><td>".$row['fullname']."</td><td>".$row['matrix_no']."</td><td>".$row['course']."</td><td>".$row['time_reg']."</td></tr>";
+		$i++;
+	}
+	$buff.= "</tbody></table></div>";
+	echo $buff;
 }
