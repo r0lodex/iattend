eb319bfdb416b85e5be1fcff436969acced8bfdb
Irfan Radzi <rolodex888@gmail.com>
2015-04-02 23:38:54 +0800

    initialized about.php

---
+++ about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>i-Attend System: About The Team</title>
+	<?php include 'static/includes/header.template'; ?>
+</head>
+<body>
+	<div class="container">
+
+		<?php $active = 'about'; include 'static/includes/navbar.template'; ?>
+		<div class="page-header text-center">
+			<h1>The Team <br><small>Meet the team behind this great web-application.</small></h1>
+		</div>
+		<div id="jumbotron-img">
+			<img src="static/img/team-cropped.jpg" width="100%;">
+		</div>
+		<div class="text-center">
+			<br>
+			<div class="well">
+				<img src="static/img/pmj1.png" class="text-center center-block" style="display:block;">
+
+				<div class="center-block">
+					<br>
+					<p class="small">
+						Politeknik Mersing Johor, Jalan Nitar 86800 Mersing, Johor Darul Ta'zim.<br>
+						<strong>Tel</strong> 07 798 0001,
+						<strong>Fax</strong> 07 798 0002,
+						<strong>Email</strong> <a href="mailto:ict@pmj.edu.my">ict@pmj.edu.my</a>
+					</p>
+					<h3>
+						<label class="small">Supervisor</label><br>
+						<strong>En. Zahaznan Bin Zaharuddin</strong>
+					</h3>
+				</div>
+				<div class="row" id="team">
+					<div class="col-md-3">
+						<h4>Mohd Ihsan Asyraf B. Izzudin Ariffin</h4>
+						<p>
+							<strong>MAT</strong> <code class="text-info bg-success">25BNS13F1007</code><br>
+							<strong>IC</strong> <code>920110-10-5615</code><br>
+							<strong>TEL</strong> <code>012-2967694</code>
+						</p>
+					</div>
+					<div class="col-md-3">
+						<h4>Noor Hafizah Bt. Sulaiman</h4>
+						<p>
+							<strong>MAT</strong> <code class="text-info bg-success">25DNS12F1002</code><br>
+						    <strong>IC</strong> <code>941201-10-5197</code><br>
+						    <strong>TEL</strong> <code>017-3451882</code>
+						</p>
+					</div>
+					<div class="col-md-3">
+						<h4>Amirul Adli B. Jafri</h4>
+						<p>
+							<strong>MAT</strong> <code class="text-info bg-success">25DNS12F1042</code><br>
+					    	<strong>IC</strong> <code>941127-01-5897</code><br>
+					    	<strong>TEL</strong> <code>017-7232726</code>
+					</div>
+					<div class="col-md-3">
+						<h4>Muhammad Amin B. Mohd Najmi</h4>
+						<p>
+							<strong>MAT</strong> <code class="text-info bg-success">25DNS12F1040</code><br>
+					    	<strong>IC</strong> <code>941201-10-5197</code><br>
+					    	<strong>TEL</strong> <code>017-3451882</code>
+					    </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
